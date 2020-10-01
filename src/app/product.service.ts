1c9b3a8d39b3d1fabb915a5d5cc3225a4104eaf2
givemo <gtchiwanza@gmail.com>
2020-10-01 19:45:26 +0100

    service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpModule } from "@angular/http";
+import { Http, Response } from "@angular/http";
 
 @Injectable()
 export class ProductService {
   constructor(private _http: Http) {}
+
+  getAlbum(id: number) {
+    return this._http.get(_ulbumUrl);
+  }
 }
